ed48b29ecff3665522c068679d27878242985ced
Maria D'Angelo <dangelo.maria@gmail.com>
2022-01-05 14:35:13 -0500

    function to get multiple tweets with relevant tweet fields

--- covid_test_finders/tweet_utils.py
+++ covid_test_finders/tweet_utils.py
@@ -3,6 +3,7 @@ import pandas as pd
 import requests
 from dotenv import load_dotenv
 from covid_test_finders.constants import BASE_URL
+from covid_test_finders.user_utils import retrieve_clean_response
 
 load_dotenv()
 # Set a .env file with your bearer token
@@ -10,37 +11,23 @@ load_dotenv()
 bearer_token = os.getenv("BEARER_TOKEN")
 
 
-
-def bearer_oauth(r):
-    """
-    Method required by bearer token authentication.
-    """
-    r.headers["Authorization"] = f"Bearer {bearer_token}"
-    r.headers["User-Agent"] = "v2UserLookupPython"
-    return r
-
-
-def get_tweet_timeline_url(user_id, max_results=20):
+def create_tweet_timeline_url(user_id, max_results=20):
     url = f"https://api.twitter.com/2/users/{user_id}/tweets?max_results={max_results}"
     print(url)
     return url
 
 
-def connect_to_endpoint(url):
-    response = requests.request("GET", url, auth=bearer_oauth,)
-    print(response.status_code)
-    if response.status_code != 200:
-        raise Exception(
-            "Request returned an error: {} {}".format(
-                response.status_code, response.text
-            )
-        )
-    return response.json()
+def create_multiple_tweets_url(tweet_ids_list):
+    tweet_ids = ','.join(tweet_ids_list) if len(tweet_ids_list)>1 else tweet_ids_list[0]
+    url = f"https://api.twitter.com/2/tweets?ids={tweet_ids}&tweet.fields=public_metrics,created_at"
+    print(url)
+    return url
 
 
-def get_user_timeline(user_id, max_results):
-    url = get_tweet_timeline_url(user_id, max_results)
-    response = connect_to_endpoint(url)
-    response_list = response.get('data')
-    response_df = pd.DataFrame(response_list)
-    return response_df
+def get_user_tweets(user_id, max_results):
+    timeline_url = create_tweet_timeline_url(user_id, max_results)
+    timeline_df = retrieve_clean_response(timeline_url)
+    tweet_ids_list = timeline_df['id'].tolist()
+    tweets_url = create_multiple_tweets_url(tweet_ids_list)
+    tweets_df = retrieve_clean_response(tweets_url)
+    return tweets_df
