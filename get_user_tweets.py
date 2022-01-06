816863ea8ff69692435fec94f124e357d67d49c8
Maria D'Angelo <dangelo.maria@gmail.com>
2022-01-06 10:39:15 -0500

    pull more data and save to disk for initial EDA

--- get_user_tweets.py
+++ get_user_tweets.py
@@ -1,3 +1,7 @@
+import pathlib
+
+from datetime import datetime as dt
+
 from covid_test_finders.constants import USER_FIELDS
 from covid_test_finders.constants import USERS_LIST
 from covid_test_finders.tweet_utils import get_user_tweets
@@ -6,7 +10,9 @@ from covid_test_finders.user_utils import get_user_info
 
 acct_name = 'COVID Test Finders'
 acct_id_col = 'id'
-max_results = 20
+max_results = 100
+output_directory = './data'
+date_str = dt.today().strftime('%Y%m%d')
 
 
 def main():
@@ -16,6 +22,10 @@ def main():
     tweets_df = get_user_tweets(user_id, max_results)
     n_rows = len(tweets_df.index)
     print(f"Retrieved {n_rows} tweets.")
+    pathlib.Path(output_directory).mkdir(parents=True, exist_ok=True)
+    output_path = f"{output_directory}/raw_tweets_{date_str}.csv"
+    print(f"Saving data to {output_path}")
+    tweets_df.to_csv(output_path)
     return tweets_df
 
 
