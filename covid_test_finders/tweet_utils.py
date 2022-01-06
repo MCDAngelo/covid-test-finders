d07037cbba58cdbb9178bc55f2cf5dd45b899583
Maria D'Angelo <dangelo.maria@gmail.com>
2022-01-06 10:01:24 -0500

    adds function to extract hashtags and function to group tweet cleaning functions

--- covid_test_finders/tweet_utils.py
+++ covid_test_finders/tweet_utils.py
@@ -10,6 +10,8 @@ load_dotenv()
 # BEARER_TOKEN=<your_bearer_token>
 bearer_token = os.getenv("BEARER_TOKEN")
 
+metadata_col='public_metrics'
+
 
 def create_tweet_timeline_url(user_id, max_results=20):
     url = f"https://api.twitter.com/2/users/{user_id}/tweets?max_results={max_results}"
@@ -24,18 +26,29 @@ def create_multiple_tweets_url(tweet_ids_list):
     return url
 
 
-def clean_tweet_metadata(df, meta_data_col='public_metrics'):
-    expanded_col = pd.json_normalize(df[meta_data_col])
+def clean_tweet_metadata(df, metadata_col=metadata_col):
+    expanded_col = pd.json_normalize(df[metadata_col])
     df = (df.join(expanded_col)
-            .drop(columns = meta_data_col))
+            .drop(columns = metadata_col))
     return df
+
+
+def get_hashtags(df):
+    df['hashtags'] = df.text.str.findall(r'#.*?(?=\s|$)')
+    return df
+
+
+def get_clean_tweets(tweet_ids_list, metadata_col):
+    tweets_url = create_multiple_tweets_url(tweet_ids_list)
+    tweets_df = retrieve_clean_response(tweets_url)
+    clean_metadata_tweets_df = clean_tweet_metadata(tweets_df)
+    clean_tweets_df = get_hashtags(clean_metadata_tweets_df)
+    return clean_tweets_df
 
 
 def get_user_tweets(user_id, max_results):
     timeline_url = create_tweet_timeline_url(user_id, max_results)
     timeline_df = retrieve_clean_response(timeline_url)
     tweet_ids_list = timeline_df['id'].tolist()
-    tweets_url = create_multiple_tweets_url(tweet_ids_list)
-    tweets_df = retrieve_clean_response(tweets_url)
-    clean_tweets_df = clean_tweet_metadata(tweets_df)
+    clean_tweets_df = get_clean_tweets(tweet_ids_list, metadata_col)
     return clean_tweets_df
